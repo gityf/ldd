7c150a2e3f4c1ea92d2563f799f8c052edb2caf5
Wang Yaofu <wangyaofu@>
2017-09-26 21:23:36 +0800

    add hello world device driver

--- hello/.hello.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/root/kernel/hello/hello.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /root/kernel/hello/hello.ko /root/kernel/hello/hello.o /root/kernel/hello/hello.mod.o
