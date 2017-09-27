653de29bf404dc4da6557483b33f50504145d237
Wang Yaofu <voipman@sina.cn>
2017-09-28 06:34:42 +0800

    comment

--- character-driver/ioc-cmd.h
+++ character-driver/ioc-cmd.h
@@ -3,10 +3,10 @@
 
 #include <linux/ioctl.h>
 
-/* 定义幻数 */
+/* to define magic number of the ioctl command */
 #define CH_DEV_IOC_MAGIC  'V'
 
-/* 定义命令 */
+/* to define ioctl command */
 #define CH_DEV_IOC_PRINT   _IO(CH_DEV_IOC_MAGIC, 1)
 #define CH_DEV_IOC_GETDATA _IOR(CH_DEV_IOC_MAGIC, 2, int)
 #define CH_DEV_IOC_SETDATA _IOW(CH_DEV_IOC_MAGIC, 3, int)
